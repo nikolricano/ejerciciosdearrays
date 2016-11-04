992784c57de1a40c5e2868b01f82685e01e47728
Nikol <nricano87@gmail.com>
2016-11-04 17:54:56 -0600

    cuartap

---
+++ cuartap.rb
@@ -0,0 +1,6 @@
+a = [ 1, 2, 3 ]
+b = [:azul, :rojo, :amarillo]
+c = [ "Tacos", "Quesadillas", "Hamburguesas"]
+
+print [b[0] = "red",b[2] = "WHITE" , c[-2] = "GREEN " ,c[1] = "RED ",b[1] = "ORANCGE",c[1] = "TANGERINE"]
+puts

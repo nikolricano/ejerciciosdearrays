45128dba0e297ad2d3fb041af6469d0f6eab0667
Nikol <nricano87@gmail.com>
2016-11-07 16:58:59 -0600

    intento de novenap

---
+++ novenap.rb
@@ -0,0 +1,10 @@
+a = [1,2,1,3,2,1,4,7,1,2].each_with_index do |number, index|
+puts "#{number}: #{index}"
+end
+
+puts "El elemento 3 está en la posición 3"
+puts "El elemento 7 está en la posición 7"
+
+#if "#{number} = #{index}"
+#  puts "la posicion #{number} = #{index}"
+#end

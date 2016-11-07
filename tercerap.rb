7524aded0228b38f0c349b3907552af63d75063b
Nikol <nricano87@gmail.com>
2016-11-07 09:06:12 -0600

    tercerap corregida

--- tercerap.rb
+++ tercerap.rb
@@ -2,4 +2,5 @@ a = [ 1, 2, 3 ]
 b = [:azul, :rojo, :amarillo]
 c = [ "Tacos", "Quesadillas", "Hamburguesas"]
 
-print a[-3]," ",b[-3]," ",c[-3]," ",a[1]," ",b[1]," ",c[1]," ",a[2}," ",b[2]," ",c[2],
+print  "#{a[0]} #{b[0]} #{c[0]}, #{a[1]} #{b[1]} #{c[1]}, #{a[2]} #{b[2]} #{c[2]}"
+puts

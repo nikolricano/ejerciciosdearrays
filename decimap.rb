a07b04824a5b14725c9e9a32acfb666cca14c350
Nikol <nricano87@gmail.com>
2016-11-07 19:28:08 -0600

    decimap

--- decimap.rb
+++ decimap.rb
@@ -1,3 +1,11 @@
+require 'pry'
 a = [1,2,3,[1,2,3],4,5,6,[1,2,3]]
+final_array = []
 
-print b= [a[3].inject(0){|sum,x| sum + x }, a[7].inject(0){|sum,x| sum + x }]
+a.each do |array_main|
+  if array_main.class == Array
+    arrays_sumas << array_main.inject(0) {|sum, x| sum += x }
+ end
+end
+
+#print b= [a[3].inject(0){|sum,x| sum + x }, a[7].inject(0){|sum,x| sum + x }]

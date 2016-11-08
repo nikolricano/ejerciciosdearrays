43ef0aded07907a2742492c7dd5f2de8c30e0877
Nikol <nricano87@gmail.com>
2016-11-07 21:54:01 -0600

    yuju quintap

--- quintap.rb
+++ quintap.rb
@@ -3,3 +3,11 @@ b = [:azul, :verde, :amarillo]
 
 #print b.group_by(&:object_id).values_at(*a).flatten(1)
 #print b.sort_by {|x| a.index x.id}
+
+final_array = []
+
+a.each do |index|
+  final_array << b[index]
+end
+
+p final_array
